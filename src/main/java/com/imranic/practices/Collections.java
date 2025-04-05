e5ab660ce84a9a3ff9b99fe52501d0709fe5afe8
Al Imran Ahmed <al.imran.cse@gmail.com>
2025-04-06 00:14:50 +0200

    Java collection and stream

--- src/main/java/com/imranic/practices/Collections.java
+++ src/main/java/com/imranic/practices/Collections.java
@@ -1,13 +1,50 @@
 package com.imranic.practices;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Collections {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+
+        // Lambda
         list.add(5);
         list.add(50);
-        System.out.println(list);
+        list.forEach(System.out::println);
+
+        // Local Date
+        LocalDate today = LocalDate.now();
+        System.out.println(today.plusDays(3));
+
+        // Stream
+        List<String> names = Arrays.asList("Bob", "Charlie", "Alice");
+
+        List<String> sortedNames = names.stream()
+                .sorted()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        System.out.println(sortedNames);
+
+        System.out.println(binarySearch(new int[]{1, 2, 3, 4, 5}, 10));
+    }
+
+    public static int binarySearch(int[] nums, int n) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == n) {
+                return mid;
+            } else if (n > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
 }
